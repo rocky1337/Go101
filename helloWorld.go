2a7b5dee4e77b9ce8e85d01ef25c12ec38688a8f
Tom Manser <Thomas_Manser@yahoo.com>
2022-07-28 21:18:44 -0500

    simple calc, need to improve

--- helloWorld.go
+++ helloWorld.go
@@ -21,9 +21,27 @@ func divide(a, b int) int {
 	return a / b
 }
 
+// TODO: Figure out how to run this in a running console app
+// TODO: Handle multiple 2+2=4  4+5= 9
+// TODO: Add a clear to restart to no numbers and what not.
+
 func main() {
 	wordPtr := flag.String("math", "foo", "a string")
+	numPtr1 := flag.Int("num1", 0, "the First Int")
+	numPtr2 := flag.Int("num2", 0, "the Second Int")
 
-	fmt.Println(*wordPtr)
+	flag.Parse()
 
+	switch *wordPtr {
+	case "add":
+		fmt.Println(add(*numPtr1, *numPtr2))
+	case "subtract":
+		fmt.Println(subtract(*numPtr1, *numPtr2))
+	case "multiply":
+		fmt.Println(multiply(*numPtr1, *numPtr2))
+	case "divide":
+		fmt.Println(divide(*numPtr1, *numPtr2))
+	default:
+		fmt.Println("Incorrect input please reconsider")
+	}
 }
