3eecbe86324ceb2e7ad29380aa4a12de3e506be4
Tom Manser <Thomas_Manser@yahoo.com>
2022-07-28 21:02:51 -0500

    Working with command line arguments

--- helloWorld.go
+++ helloWorld.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"math"
 )
 
+func add(a, b int) int {
+	return a + b
+}
+
+func subtract(a, b int) int {
+	return a - b
+}
+
+func multiply(a, b int) int {
+	return a * b
+}
+
+func divide(a, b int) int {
+	return a / b
+}
+
 func main() {
-	fmt.Println(math.Pi)
+	wordPtr := flag.String("math", "foo", "a string")
+
+	fmt.Println(*wordPtr)
 
 }
