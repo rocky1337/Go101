b30b27ba03a28f346cc5c369ad057f3e683d469d
Tom Manser <Thomas_Manser@yahoo.com>
2022-07-28 21:54:59 -0500

    eh fuck it

--- helloWorld.go
+++ helloWorld.go
@@ -24,24 +24,28 @@ func divide(a, b int) int {
 // TODO: Figure out how to run this in a running console app
 // TODO: Handle multiple 2+2=4  4+5= 9
 // TODO: Add a clear to restart to no numbers and what not.
-
 func main() {
+	// Inputs
+	var total int
 	wordPtr := flag.String("math", "foo", "a string")
 	numPtr1 := flag.Int("num1", 0, "the First Int")
 	numPtr2 := flag.Int("num2", 0, "the Second Int")
-
+	// Must parse to get actual value of the pointers to the inputs
 	flag.Parse()
 
 	switch *wordPtr {
 	case "add":
-		fmt.Println(add(*numPtr1, *numPtr2))
+		total = add(*numPtr1, *numPtr2)
 	case "subtract":
-		fmt.Println(subtract(*numPtr1, *numPtr2))
+		total = subtract(*numPtr1, *numPtr2)
 	case "multiply":
-		fmt.Println(multiply(*numPtr1, *numPtr2))
+		total = multiply(*numPtr1, *numPtr2)
 	case "divide":
-		fmt.Println(divide(*numPtr1, *numPtr2))
+		total = divide(*numPtr1, *numPtr2)
 	default:
-		fmt.Println("Incorrect input please reconsider")
+		fmt.Println("Incorrect input please reconsider your life choices")
 	}
+
+	fmt.Println(total)
+
 }
